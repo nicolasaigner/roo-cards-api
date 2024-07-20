dc43599a61395f8618704ae70dc75957f5c5face
Nícolas Aigner <nicolas.aigner@gmail.com>
2024-07-20 02:58:17 -0300

    Ajustes e melhores nos dados e script da API

--- api/index.js
+++ api/index.js
@@ -1,19 +1,41 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 const app = express();
+
+app.use(cors({
+    origin: '*'
+}));
 
 let cards = [];
 let mvps = [];
+let recommendationsCards = {};
 let packageJson = {};
+
+const armamentoToFolder = {
+    "Acessório": "acessorio",
+    "Armadura": "armadura",
+    "Boca": "boca",
+    "Calçado": "calcado",
+    "Capa": "capa",
+    "Chapéu": "chapeu",
+    "Costas": "costas",
+    "Face": "face",
+    "Fantasia": "fantasia",
+    "Mão Dominante": "mao_dominante",
+    "Mão Secundária": "mao_secundaria"
+};
 
 try {
     const cardsPath = path.join(__dirname, 'cards.json');
     const mvpsPath = path.join(__dirname, 'mvps.json');
+    const recommendationsCardsPath = path.join(__dirname, 'recommendations_cards.json');
     const packagePath = path.join(__dirname, '../package.json');
 
     cards = JSON.parse(fs.readFileSync(cardsPath, 'utf8'));
     mvps = JSON.parse(fs.readFileSync(mvpsPath, 'utf8'));
+    recommendationsCards = JSON.parse(fs.readFileSync(recommendationsCardsPath, 'utf8'));
     packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
 } catch (e) {
     console.error('Erro ao ler os arquivos JSON:', e);
@@ -35,23 +57,19 @@ function determineAvailableFilters(data) {
 }
 
 function filterByQuery(data, query) {
-    return data.filter(item => {
-        return Object.keys(query).every(key => {
-            return item[key] && item[key].toString().toLowerCase() === query[key].toString().toLowerCase();
-        });
+    const correctedQuery = {};
+    Object.keys(query).forEach(key => {
+        const correctedKey = key.charAt(0).toUpperCase() + key.slice(1).replace('_', ' ');
+        correctedQuery[correctedKey] = query[key];
     });
-}
-
-async function attachImagesToCards(cards) {
-    const album = await client.getAlbum('TJAAMyH');
-    const images = album.data.images;
-
-    return cards.map(card => {
-        const image = images.find(img => img.description === card.name);
-        if (image) {
-            card.imageUrl = image.link;
-        }
-        return card;
+    return data.filter(item => {
+        return Object.keys(correctedQuery).every(key => {
+            const itemKey = Object.keys(item).find(k => k.toLowerCase() === key.toLowerCase());
+            if (!itemKey) return false;
+            const itemValue = item[itemKey] ? item[itemKey].toString().toLowerCase() : '';
+            const queryValue = correctedQuery[key] ? correctedQuery[key].toString().toLowerCase() : '';
+            return itemValue === queryValue;
+        });
     });
 }
 
@@ -86,24 +104,37 @@ app.get('/cards', async (req, res) => {
     res.json(cardsWithImages);
 });
 
-const armamentoToFolder = {
-    "Acessório": "acessorio",
-    "Armadura": "armadura",
-    "Boca": "boca",
-    "Calçado": "calcado",
-    "Capa": "capa",
-    "Chapéu": "chapeu",
-    "Costas": "costas",
-    "Face": "face",
-    "Fantasia": "fantasia",
-    "Mão Dominante": "mao_dominante",
-    "Mão Secundária": "mao_secundaria"
-};
-
 app.use('/card/image', express.static(path.join(__dirname, '../database/images/cartas')));
 
-app.get('/mvps', (req, res) => {
-    res.json(filterByQuery(mvps, req.query));
+app.get('/mvps', async (req, res) => {
+    const filteredMvps = filterByQuery(mvps, req.query);
+    const baseUrl = `${req.protocol}://${req.get('host')}/mvp/image`;
+
+    const mvpsWithImages = filteredMvps.map(mvp => {
+        const imageName = `${mvp.name}.png`;
+        let conversorArray = [];
+        if (mvp.hasOwnProperty('recommended_elements')) {
+            mvp.recommended_elements.forEach((item) => {
+                conversorArray.push(`${req.protocol}://${req.get('host')}/conversor/image/${item}.png`);
+            });
+        }
+
+        return {
+            ...mvp,
+            thumbnailUrl: `${baseUrl}/thumbnails/${imageName}`,
+            imageUrl: `${baseUrl}/total/${imageName}`,
+            conversorImageUrl: conversorArray,
+        };
+    });
+
+    res.json(mvpsWithImages);
 });
+
+app.get('/recommendations', async (req, res) => {
+    res.json(recommendationsCards);
+});
+
+app.use('/mvp/image', express.static(path.join(__dirname, '../database/images/monstros/mvp')));
+app.use('/conversor/image', express.static(path.join(__dirname, '../database/images/conversores')));
 
 module.exports = app;
